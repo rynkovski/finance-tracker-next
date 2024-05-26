cbb35eb952fd5c11e82474f9661727a7812cb835
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-05-26 22:50:33 +0200

    feat: add tabs to categories

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import AddModal from "@/components/add-modal";
 import AddCategoriesModal from "@/components/categories/add-categories-modal";
 import CategoriesList from "@/components/categories/categories-list";
@@ -13,6 +14,9 @@ import { Box, Typography } from "@mui/material";
 import Container from "@mui/material/Container";
 import WorkIcon from "@mui/icons-material/Work";
 import ScatterPlotIcon from "@mui/icons-material/ScatterPlot";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { SetStateAction, useState } from "react";
 
 const incomes = [
   {
@@ -53,7 +57,51 @@ const expenses = [
   },
 ];
 
+function CustomTabPanel(props: {
+  [x: string]: any;
+  children: any;
+  value: any;
+  index: any;
+}) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Box
+      width={"100%"}
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </Box>
+  );
+}
+
+// CustomTabPanel.propTypes = {
+//   children: PropTypes.node,
+//   index: PropTypes.number.isRequired,
+//   value: PropTypes.number.isRequired,
+// };
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
 function Categories() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: any, newValue: SetStateAction<number>) => {
+    setValue(newValue);
+  };
   return (
     <Container fixed>
       <Box
@@ -66,19 +114,16 @@ function Categories() {
         width={"100%"}
       >
         <AddModal children={<AddCategoriesModal />} />
-        <Typography
-          variant="h4"
-          color="green"
-          fontWeight={700}
-          letterSpacing={3}
-        >
-          Incomes
-        </Typography>
-        <CategoriesList categories={incomes} />
-        <Typography variant="h4" color="red" fontWeight={700} letterSpacing={3}>
-          Expenses
-        </Typography>
-        <CategoriesList categories={expenses} />
+        <Tabs value={value} onChange={handleChange} centered>
+          <Tab label="Income" />
+          <Tab label="Expense" />
+        </Tabs>
+        <CustomTabPanel value={value} index={0}>
+          <CategoriesList categories={incomes} />
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          <CategoriesList categories={expenses} />
+        </CustomTabPanel>
       </Box>
     </Container>
   );
