a6c4e1929047905e8b57865837964306e90f5c82
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-05-27 20:31:09 +0200

    feat: change dashboard cards

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -2,17 +2,30 @@
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import Deposits from "@/components/deposits";
-import Orders from "@/components/orders";
 import Copyright from "@/components/copyright";
-import Chart from "@/components/dashboard/chart";
+import Chart, { SimpleLineChart } from "@/components/dashboard/chart";
+import BasicCard from "@/components/dashboard/basic-card";
 
-export default async function Dashboard() {
+export default function Dashboard() {
   return (
     <>
       <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
         <Grid container spacing={3}>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12} md={4}>
+            <BasicCard />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <BasicCard />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <BasicCard />
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+              <SimpleLineChart />
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={4}>
             <Paper
               sx={{
                 p: 2,
@@ -22,23 +35,6 @@ export default async function Dashboard() {
               }}
             >
               <Chart />
-            </Paper>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Paper
-              sx={{
-                p: 2,
-                display: "flex",
-                flexDirection: "column",
-                height: 240,
-              }}
-            >
-              <Deposits />
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-              <Orders />
             </Paper>
           </Grid>
         </Grid>
--- src/components/dashboard/chart.tsx
+++ src/components/dashboard/chart.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import { PieChart } from "@mui/x-charts/PieChart";
+import { LineChart } from "@mui/x-charts/LineChart";
 
 export default function BasicPie() {
   return (
     <PieChart
+      sx={{ display: "flex", flexDirection: "column" }}
       colors={["lightgreen", "green", "teal"]}
       series={[
         {
@@ -16,6 +18,32 @@ export default function BasicPie() {
       ]}
       width={400}
       height={200}
+    />
+  );
+}
+
+const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+const xLabels = [
+  "Page A",
+  "Page B",
+  "Page C",
+  "Page D",
+  "Page E",
+  "Page F",
+  "Page G",
+];
+
+export function SimpleLineChart() {
+  return (
+    <LineChart
+      width={500}
+      height={300}
+      series={[
+        { data: pData, label: "pv" },
+        { data: uData, label: "uv" },
+      ]}
+      xAxis={[{ scaleType: "point", data: xLabels }]}
     />
   );
 }
