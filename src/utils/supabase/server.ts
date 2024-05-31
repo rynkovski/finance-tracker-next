8685bf3758ddf01b72cd8046a0893d06dd49a7e8
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-05-31 19:30:42 +0200

    fix: clerk supabase integration

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,9 +1,7 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export function createClient() {
-  const cookieStore = cookies();
-
+export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -33,4 +31,4 @@ export function createClient() {
       },
     }
   );
-}
+};
